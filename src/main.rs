c4bd5dc1b304c93525e4a06112d06f061c5a473e
Tastaturtaste <joshua.moellers@gmx.net>
2021-05-16 02:43:30 +0200

    Initial window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{EventLoop, ControlFlow},
+    window::WindowBuilder,
+};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Game of Life")
+        .build(&event_loop)
+        .expect("Window creation failed!");
+
+    event_loop.run(
+        move |event, _, control_flow| {
+            *control_flow = ControlFlow::Poll;
+
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested, .. 
+                } => {
+                    println!("The close button was pressed; stopping");
+                    *control_flow = ControlFlow::Exit
+                },
+                Event::MainEventsCleared => {
+                    window.request_redraw();
+                },
+                _ => ()
+            }
+        }
+    );
 }
